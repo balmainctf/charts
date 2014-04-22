50dadf6df28db916691aea860b60c68ad125506c
cuiken <ken.cuik@gmail.com>
2014-04-22 16:58:12 +0800

    显示图表

--- app.js
+++ app.js
@@ -8,13 +8,33 @@ var path = require('path');
 
 var app = express();
 
+// all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-http.createServer(app).listen(app.get('port'), function () {
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/',function(req,res){
+    res.render('index',{});
+});
+
+app.get('/stationDy',function(req,res){
+    res.render('pie-stationDy');
+})
+
+http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
