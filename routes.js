b72836d5de08ab753604c636b140493e326bf326
cuiken <ken.cuik@gmail.com>
2014-05-06 13:02:39 +0800

    ...

--- routes.js
+++ routes.js
@@ -24,6 +24,23 @@ module.exports = function (app) {
         res.render('co2Avoided', {title: "CO2 Avoided", period: period, format: date.format, currDate: date.value});
     });
 
+    app.get('/station_env', function (req, res) {
+
+        res.render('station_env', {title: 'Station Event'});
+    });
+
+    app.get('/station_evn_info', function (req, res, next) {
+        var sdate = req.query.sdate;
+        var edate = req.query.edate;
+        var url = config.host + 'stationEventInfo?key=' + config.station_key + '&sdt=' + sdate + '&edt=' + edate;
+        urllib.request(url, {dataType: 'json'}, function (err, data) {
+            if (err) {
+                return next(err);
+            }
+            res.json(data);
+        });
+    });
+
     app.get('/co2AvoidedInfo/:period/:date', function (req, res, next) {
         var period = req.params.period;
         var date = req.params.date;
