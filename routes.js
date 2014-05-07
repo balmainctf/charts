bc298e844629193382c5807b8deaeb1dabd7ad81
cuiken <ken.cuik@gmail.com>
2014-05-07 10:12:25 +0800

    ...fixed

--- routes.js
+++ routes.js
@@ -81,28 +81,21 @@ module.exports = function (app) {
             }
             res.json(data);
         });
-    })
+    });
 
-    app.get('/', function (req, res, next) {
+    app.get('/', function (req, res) {
 
+        res.render('home', {title: 'Station Dynamic Info'});
+
+    });
+
+    app.get('/stationDyInfo', function (req, res, next) {
         var url = config.host + 'stationDynamicInfo?key=' + config.station_key;
-
         urllib.request(url, {dataType: 'json'}, function (err, data) {
             if (err) {
                 return next(err);
             }
-            var dynamicInfo = data;
-            res.render('home', {
-                    title: 'home',
-                    "e_total": dynamicInfo['E-Total'].value + ' ' + dynamicInfo['E-Total'].unit,
-                    "e_today": dynamicInfo['E-Today'].value + ' ' + dynamicInfo['E-Today'].unit,
-                    "total_yield": dynamicInfo['TotalYield'].value + ' ' + dynamicInfo['TotalYield'].unit,
-                    "e_month": dynamicInfo['E-Month'].value + ' ' + dynamicInfo['E-Month'].unit,
-                    "co2Avoided": dynamicInfo['CO2Avoided'].value + ' ' + dynamicInfo['CO2Avoided'].unit,
-                    "ludt": dynamicInfo.ludt
-                }
-            );
+            res.json(data);
         });
-
     });
 }
